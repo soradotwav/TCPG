87cf90cafef3dff58c0ddf3b899401fe4abec708
soradotwav <github.soradotwav.com>
2023-12-24 02:25:46 -0800

    The Defense Of Consolas

---
+++ Chapter 8/TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Title = "Defense of Consolas";
+
+Console.Write("Target Row: ");
+var targetRow = Convert.ToByte(Console.ReadLine());
+
+Console.Write("Target Column: ");
+var targetColumn = Convert.ToByte(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("(" + targetRow + ", " + Math.Clamp(targetColumn - 1, (byte)0, (byte)8) + ")");
+Console.WriteLine("(" + Math.Clamp(targetRow - 1, (byte)0, (byte)8) + ", " + targetColumn + ")");
+Console.WriteLine("(" + targetRow + ", " + Math.Clamp(targetColumn + 1, (byte)0, (byte)8) + ")");
+Console.WriteLine("(" + Math.Clamp(targetRow + 1, (byte)0, (byte)8) + ", " + targetColumn + ")");
+
+//Console.Beep(400, 500); --> Commented out due to it being unavailable on macOS
