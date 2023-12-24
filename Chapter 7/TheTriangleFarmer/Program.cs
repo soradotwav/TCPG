180e605e9d882d71bc7c4fac4596ee32a115f242
soradotwav <github.soradotwav.com>
2023-12-24 01:20:34 -0800

    The Triangle Farmer

---
+++ Chapter 7/TheTriangleFarmer/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Triangle base: ");
+var triangleBase = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Triangle height: ");
+var triangleHeight = Convert.ToInt32(Console.ReadLine());
+
+var triangleArea = (triangleBase * triangleHeight) / 2d;
+
+Console.WriteLine("The area of the triangle is " + triangleArea);
