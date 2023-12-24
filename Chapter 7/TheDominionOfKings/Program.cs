d1deba74b25dcb78f0d190cb2234a46fe5670d16
soradotwav <github.soradotwav.com>
2023-12-24 01:44:08 -0800

    The Dominion of Kings

---
+++ Chapter 7/TheDominionOfKings/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("How many provinces do you have?");
+Console.Write("> ");
+var numProvinces = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many duchies do you have?");
+Console.Write("> ");
+var numDuchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many estates do you have?");
+Console.Write("> ");
+var numEstates = Convert.ToInt32(Console.ReadLine());
+
+var totalScore = numProvinces * 6 + numEstates + numDuchies * 3;
+
+Console.WriteLine("Your total score is " + totalScore + ".");
