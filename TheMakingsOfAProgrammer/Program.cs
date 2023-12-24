0fc3f497a58ed10dd0cbb3d22c89893efcf1f5b4
soradotwav <soradesignz@gmail.com>
2023-12-23 17:08:14 -0800

    The Makings of a Programmer

---
+++ TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Question: How many statements do you think a program can contain?\n");
+Console.WriteLine("I believe a program in C# can contain unlimited statements");
+Console.WriteLine("as the program can technically contain unlimited lines.");
+Console.WriteLine("While this is possible, I think that it is best to keep the number of");
+Console.WriteLine("lines to a minimum within the scope of the project to improve performance.");
