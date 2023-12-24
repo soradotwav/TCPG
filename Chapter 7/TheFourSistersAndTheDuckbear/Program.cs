33f5126a76bb92265c8f38d538a38ef420de7f98
soradotwav <github.soradotwav.com>
2023-12-24 01:32:05 -0800

    The Four Sisters and the Duckbear

---
+++ Chapter 7/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("How many chocolate eggs were gathered today?");
+Console.Write("> ");
+
+var eggsCollected = Convert.ToInt32(Console.ReadLine());
+
+var eggsForDuckbear = eggsCollected % 4;
+var eggsForEachSister = (eggsCollected - eggsForDuckbear) / 4;
+
+Console.WriteLine("Each of the sisters should get " + eggsForEachSister + " and the Duckbear the remaining " + eggsForDuckbear + " eggs.");
