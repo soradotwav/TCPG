f5c46c5a1461056dde4a8efb69fa8edb09c87f8e
soradotwav <dev@soradotwav.com>
2023-12-27 20:13:20 -0800

    Finished Chapter 16
    - Finished Simula's Test

---
+++ Chapter 16/SimulasTest/Program.cs
@@ -0,0 +1,35 @@
+﻿// Set initial state as "locked"
+ChestState current = ChestState.Locked;
+
+// Main loop; runs until an empty string is entered
+while(true)
+{
+    Console.Write("The chest is " + current.ToString().ToLower() + ". What do you want to do? ");
+    var userIn = Console.ReadLine();
+
+    if (userIn.Equals("")) break;
+    EvaluateUserInput(userIn);
+}
+
+// Evaluates the user input and updates the chest state accordingly.
+void EvaluateUserInput(String input)
+{
+    if (current == ChestState.Locked && EqualsIgnoreCase(input, "unlock") ||
+        current == ChestState.Open && EqualsIgnoreCase(input, "close"))
+        current = ChestState.Closed;
+    else if (current == ChestState.Closed)
+    {
+        if (EqualsIgnoreCase(input, "lock"))
+            current = ChestState.Locked;
+        else if (EqualsIgnoreCase(input, "open"))
+            current = ChestState.Open;
+    }
+}
+
+// Helper function to compare two strings without considering case sensitivity.
+bool EqualsIgnoreCase(string input, string input2) => String.Equals(input, input2, StringComparison.OrdinalIgnoreCase);
+
+/// <summary>
+/// Represents the possible states of the chest.
+/// </summary>
+enum ChestState {Open, Closed, Locked}
